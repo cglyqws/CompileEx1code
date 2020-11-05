1c75f207036e02fb40ad361944b36c7b7911359d
zzy <512063422@qq.com>
2020-11-05 11:31:16 +0800

    test

--- analyse.c
+++ analyse.c
@@ -3,12 +3,13 @@
 #include<ctype.h>
 #include <stdlib.h>
 char stmp[1005];
-char stack1[500];
-char stack2[500];
-int top1,top2;
+char stack[1006];
+char stackn[1006];
+char topchar,topn;
+int top;
 int tempint;
 int i;
-int priority[106][106];
+int priority[110][110];
 void initpriority()
 {
 	//1 > -1 < -2 Пе 2 =   
@@ -41,67 +42,139 @@ void initpriority()
 	priority[')']['i'] = -2;
 	priority[')']['('] = -2;
 	priority[')'][')'] = 1;
+	
+	priority['#']['+'] = -1;
+	priority['#']['*'] = -1;
+	priority['#']['i'] = -1;
+	priority['#']['('] = -1;
+	priority['#'][')'] = -1;
+	priority['#']['#'] = 0;
+	
+	priority[')']['#'] = 1;
+	priority['i']['#'] = 1;
+	priority['+']['#'] = 1;
+	priority['*']['#'] = 1;
 }
 int main (int argc, char *argv[])
 {
 	initpriority();
 	
-	FILE *in = fopen(argv[1],"rt");
-	
-	int size = sizeof(stmp) ;
-		
-	while  (fgets(stmp,1000,in))
-	{
-		for (i=0;stmp[i]!='\r'&&stmp[i]!='\n';i++)
+//	FILE *in = fopen(argv[1],"rt");
+//	
+//	while  (fgets(stmp,1000,in))
+//	{
+//		for (i=0;stmp[i]!='\r'&&stmp[i]!='\n';i++)
+//		{
+//			if (!top) topchar = '#';
+//			else topchar = stack[top-1];
+//			
+//			switch (priority[topchar][stmp[i]])
+//			{
+//				case 1:
+//					while (priority[topchar][stmp[i]]==1)
+//					{
+//						if (topchar == 'i')
+//						{
+//							top--;
+//							topn++;
+//							printf("R\n");
+//						}
+//						else if (topchar == '+'||topchar == '*')
+//						{
+//							if (topn>1)
+//							{
+//								topn--;
+//								top--;
+//							}
+//							else {
+//								printf("RE\n");
+//							}
+//						}
+//						else if (topchar == ')')
+//						{
+//							top--;
+//						}
+//						if (!top) topchar = '#';
+//						else topchar = stack[top-1];
+//					}
+//					break;
+//				case 2:
+//					top--;
+//					break;
+//				case 0:
+//					printf("E\n");
+//					return 0;
+//					
+//				case -1:
+//					stack[top++] = stmp[i];
+//					printf("I%c\n",stmp[i]);
+//					break;
+//				case -2:
+//					printf("E\n");
+//					return 0;
+//				 
+//			}
+//		}
+//		memset(stmp, 0, sizeof(stmp));
+//		memset(stack, 0,sizeof(stack));	
+//		top = 0;
+//	}
+	scanf("%s",stmp);
+	for (i=0;;i++)
 		{
-			if (stmp[i] == 'i')
+			if (!top) topchar = '#';
+			else topchar = stack[top-1];
+			if (stmp[i]==0)
 			{
-				stack1[top1++] = 'i';
-				printf("Ii\n");
-				continue;
+				stmp[i] = '#';
 			}
-			else 
+			switch (priority[topchar][stmp[i]])
 			{
-				switch (priority[stack2[top2-1]][stmp[i]])
-				{
-					case 1:
-						while (priority[stack2[top2-1]][stmp[i]]==1)
+				case 1:
+					while (priority[topchar][stmp[i]]==1)
+					{
+						if (topchar == 'i')
 						{
-							if (top1>1)
+							top--;
+							topn++;
+							printf("R\n");
+						}
+						else if (topchar == '+'||topchar == '*')
+						{
+							if (topn>1)
 							{
-								top1--;
-								top2--;
+								topn--;
+								top--;
 								printf("R\n");
-								if (!top2) break;
 							}
-							else 
-							{
+							else {
 								printf("RE\n");
 								return 0;
 							}
 						}
-						break;
-					case 2:
-						top2--;
-						break;
-					case -1:
-						stack2[top2++] = stmp[i];
-						printf("I%c\n",stmp[i]);
-						break;
-					case -2:
-						printf("E\n");
-						return 0;
-				}
+						else if (topchar == ')')
+						{
+							top--;
+						}
+						if (!top) topchar = '#';
+						else topchar = stack[top-1];
+					}
+					i--;
+					break;
+				case 2:
+					top--;
+					break;
+				case 0:
+					return 0;
+					
+				case -1:
+					stack[top++] = stmp[i];
+					printf("I%c\n",stmp[i]);
+					break;
+				case -2:
+					printf("E\n");
+					return 0;
+				 
 			}
-			
 		}
-		
-
-		memset(stmp, 0, sizeof(stmp));
-		memset(stack1, 0,sizeof(stack1));
-		memset(stack2, 0,sizeof(stack2));
-		top1 = 0;
-		top2 = 0;
-	}
-	
 }
