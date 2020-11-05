16162492520df1c837a018c345f5ae8847212b8d
zzy <512063422@qq.com>
2020-11-05 11:48:45 +0800

    test

--- analyse.c
+++ analyse.c
@@ -59,11 +59,73 @@ int main (int argc, char *argv[])
 {
 	initpriority();
 	
-	FILE *in = fopen(argv[1],"rt");
-	
-	while  (fgets(stmp,1000,in))
-	{
-		for (i=0;;i++)
+//	FILE *in = fopen(argv[1],"rt");
+//	
+//	while  (fgets(stmp,1000,in))
+//	{
+//		for (i=0;;i++)
+//		{
+//			if (!top) topchar = '#';
+//			else topchar = stack[top-1];
+//			if (stmp[i]==0)
+//			{
+//				stmp[i] = '#';
+//			}
+//			switch (priority[topchar][stmp[i]])
+//			{
+//				case 1:
+//					while (priority[topchar][stmp[i]]==1)
+//					{
+//						if (topchar == 'i')
+//						{
+//							top--;
+//							topn++;
+//							printf("R\n");
+//						}
+//						else if (topchar == '+'||topchar == '*')
+//						{
+//							if (topn>1)
+//							{
+//								topn--;
+//								top--;
+//								printf("R\n");
+//							}
+//							else {
+//								printf("RE\n");
+//								return 0;
+//							}
+//						}
+//						else if (topchar == ')')
+//						{
+//							top--;
+//						}
+//						if (!top) topchar = '#';
+//						else topchar = stack[top-1];
+//					}
+//					i--;
+//					break;
+//				case 2:
+//					top--;
+//					break;
+//				case 0:
+//					return 0;
+//					
+//				case -1:
+//					stack[top++] = stmp[i];
+//					printf("I%c\n",stmp[i]);
+//					break;
+//				case -2:
+//					printf("E\n");
+//					return 0;
+//				 
+//			}
+//		}
+//		memset(stmp, 0, sizeof(stmp));
+//		memset(stack, 0,sizeof(stack));	
+//		top = 0;
+//	}
+	scanf("%s",stmp);
+	for (i=0;;i++)
 		{
 			if (!top) topchar = '#';
 			else topchar = stack[top-1];
@@ -97,7 +159,8 @@ int main (int argc, char *argv[])
 						}
 						else if (topchar == ')')
 						{
-							top--;
+							top-=2;
+							printf("R\n");
 						}
 						if (!top) topchar = '#';
 						else topchar = stack[top-1];
@@ -105,7 +168,8 @@ int main (int argc, char *argv[])
 					i--;
 					break;
 				case 2:
-					top--;
+					stack[top++] = stmp[i];
+					printf("I%c\n",stmp[i]);
 					break;
 				case 0:
 					return 0;
@@ -120,10 +184,5 @@ int main (int argc, char *argv[])
 				 
 			}
 		}
-		memset(stmp, 0, sizeof(stmp));
-		memset(stack, 0,sizeof(stack));	
-		top = 0;
-	}
-//	scanf("%s",stmp);
 	
 }
